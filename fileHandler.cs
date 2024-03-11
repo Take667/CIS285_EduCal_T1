989088e26a9029be8f66801349f2ddd752965f14
joshSawYou <122572331+joshSawYou@users.noreply.github.com>
2024-03-10 23:50:25 -0400

    Update fileHandler.cs

--- fileHandler.cs
+++ fileHandler.cs
@@ -1,66 +1,27 @@
 using System.IO;
 using System.Text.Json;
-using System.Xml.Serialization;
 
 namespace EduCal
 {
     public static class FileHelper
     {
-        public static void SaveToJsonFile<T>(string fileName, T data)
+        public static void SaveToFile(string fileName, object data)
         {
             string jsonString = JsonSerializer.Serialize(data);
+
+            // Write the JSON string to a file
             File.WriteAllText(fileName, jsonString);
         }
 
-        public static T LoadFromJsonFile<T>(string fileName)
+        public static T LoadFromFile<T>(string fileName)
         {
+            // Read the JSON string from the file
             string jsonString = File.ReadAllText(fileName);
-            return JsonSerializer.Deserialize<T>(jsonString);
-        }
 
-        public static void SaveToXmlFile<T>(string fileName, T data)
-        {
-            var serializer = new XmlSerializer(typeof(T));
+            // Deserialize the JSON string to the specified type
+            T data = JsonSerializer.Deserialize<T>(jsonString);
 
-            using (var writer = new StreamWriter(fileName))
-            {
-                serializer.Serialize(writer, data);
-            }
-        }
-
-        public static T LoadFromXmlFile<T>(string fileName)
-        {
-            var serializer = new XmlSerializer(typeof(T));
-
-            using (var reader = new StreamReader(fileName))
-            {
-                return (T)serializer.Deserialize(reader);
-            }
+            return data;
         }
     }
 }
-
-class Program
-{
-    static void Main()
-    {
-        // Example of saving settings to JSON file
-        EventSettings eventSettings = new EventSettings
-        {
-            // ... (your settings)
-        };
-
-        FileHelper.SaveToJsonFile("app_settings.json", eventSettings);
-
-        // Example of loading settings from JSON file
-        EventSettings loadedJsonSettings = FileHelper.LoadFromJsonFile<EventSettings>("app_settings.json");
-
-        // Example of saving settings to XML file
-        FileHelper.SaveToXmlFile("app_settings.xml", eventSettings);
-
-        // Example of loading settings from XML file
-        EventSettings loadedXmlSettings = FileHelper.LoadFromXmlFile<EventSettings>("app_settings.xml");
-    }
-}
-
-
