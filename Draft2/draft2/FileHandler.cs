7d1e4e4454b1700e53f5395afae8e7bcdc73b638
Take667 <156635062+Take667@users.noreply.github.com>
2024-04-11 16:06:27 -0500

    Add files via upload
    Added some stuff to draft 2

---
+++ Draft2/draft2/FileHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace EduCal
+{
+    public static class FileHelper
+    {
+        public static void SaveToFile<T>(string fileName, T data)
+        {
+            // Create an XmlSerializer for the specified type
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+
+            // Create a StreamWriter to write the XML data to the file
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                // Serialize the data to XML and write it to the file
+                serializer.Serialize(writer, data);
+            }
+        }
+
+        public static T LoadFromFile<T>(string fileName)
+        {
+            // Create an XmlSerializer for the specified type
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+
+            // Create a StreamReader to read the XML data from the file
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                // Deserialize the XML data to the specified type
+                T data = (T)serializer.Deserialize(reader);
+                return data;
+            }
+        }
+    }
+}
