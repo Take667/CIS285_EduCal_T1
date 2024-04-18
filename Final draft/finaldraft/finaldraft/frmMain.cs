64731e956afae921245a7524b50d774ee3b5dc10
Take667 <156635062+Take667@users.noreply.github.com>
2024-04-18 17:08:33 -0500

    Add files via upload

---
+++ Final draft/finaldraft/finaldraft/frmMain.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EduCal {
+	public partial class frmMain : Form {
+        private object calendarControl;
+
+        public frmMain () {
+			InitializeComponent ();
+		}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Initialize the calendar to the current date
+            calendarControl.SetDate(DateTime.Today);
+
+
+            private void btnPreviousMonth_Click(object sender, EventArgs e)
+        {
+           
+                // Get the current displayed date
+                DateTime currentDisplayedDate = calendarControl.SelectionStart;
+
+                // Calculate the previous month
+                DateTime previousMonth = currentDisplayedDate.AddMonths(-1);
+
+            }
+
+        private void btnNextMonth_Click(object sender, EventArgs e)
+        {
+            
+            {
+                // Get the current displayed date
+                DateTime currentDisplayedDate = calendarControl.SelectionStart;
+
+                // Calculate the next month
+                DateTime nextMonth = currentDisplayedDate.AddMonths(1);
+
+            }
+
+            private void label3_Click(object sender, EventArgs e)
+        {
+            
+           
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void panel20_Paint(object sender, PaintEventArgs e)
+        {
+            
+           
+        }
+
+        
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        
+        private void panel14_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+        
+        
+    }
+}
