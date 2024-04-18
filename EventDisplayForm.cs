0c26b3f2d178d2bc8c1c99ae68902ad702b134ac
Take667 <156635062+Take667@users.noreply.github.com>
2024-04-18 15:33:05 -0500

    Update EventDisplayForm.cs

--- EventDisplayForm.cs
+++ EventDisplayForm.cs
@@ -25,21 +25,7 @@ namespace EduCal
 
         // Event Fields
 
-        public string Title { get; set; }
-        public string ShortDescription { get; set; }
-        public string LongDescription { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime? EndDate { get; set; } // Nullable if applicable
-        public string Location { get; set; }
-
-        // New properties for event repetition
-        public RecurrencePattern Recurrence { get; set; }
-
-        // Color coordination properties
-        public string Color { get; set; }
-
-        // Categorization properties
-        public List<string> Categories { get; set; } = new List<string>();
+        
 
         public EventForm()
         {
