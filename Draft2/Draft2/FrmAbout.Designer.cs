6d56ef80bae7e2e7502650093f52128af63f9e26
Take667 <156635062+Take667@users.noreply.github.com>
2024-04-18 12:29:45 -0500

    Add files via upload

---
+++ Draft2/Draft2/FrmAbout.Designer.cs
@@ -0,0 +1,168 @@
+﻿using System.Windows.Forms;
+
+namespace EduCal
+{
+    partial class frmAbout : Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuEdit;
+        private System.Windows.Forms.ToolStripMenuItem mnuAbout;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private System.Windows.Forms.Label labelProductName;
+        private System.Windows.Forms.Label labelVersion;
+        private System.Windows.Forms.TextBox textBoxDescription;
+
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuEdit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.labelProductName = new System.Windows.Forms.Label();
+            this.labelVersion = new System.Windows.Forms.Label();
+            this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.tableLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(32, 19);
+            // 
+            // mnuEdit
+            // 
+            this.mnuEdit.Name = "mnuEdit";
+            this.mnuEdit.Size = new System.Drawing.Size(32, 19);
+            // 
+            // mnuAbout
+            // 
+            this.mnuAbout.Name = "mnuAbout";
+            this.mnuAbout.Size = new System.Drawing.Size(32, 19);
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.ColumnCount = 2;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 67F));
+            this.tableLayoutPanel.Controls.Add(this.labelProductName, 1, 1);
+            this.tableLayoutPanel.Controls.Add(this.labelVersion, 1, 2);
+            this.tableLayoutPanel.Controls.Add(this.textBoxDescription, 1, 4);
+            this.tableLayoutPanel.Controls.Add(this.btnClose, 1, 5);
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(14, 14);
+            this.tableLayoutPanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 6;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.80368F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.828221F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(701, 572);
+            this.tableLayoutPanel.TabIndex = 0;
+            this.tableLayoutPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel_Paint);
+            // 
+            // labelProductName
+            // 
+            this.labelProductName.AutoSize = true;
+            this.labelProductName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelProductName.Location = new System.Drawing.Point(240, 79);
+            this.labelProductName.Margin = new System.Windows.Forms.Padding(9, 0, 4, 0);
+            this.labelProductName.MaximumSize = new System.Drawing.Size(0, 49);
+            this.labelProductName.Name = "labelProductName";
+            this.labelProductName.Size = new System.Drawing.Size(457, 33);
+            this.labelProductName.TabIndex = 19;
+            this.labelProductName.Text = "Education Calendar\r\n";
+            this.labelProductName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelVersion
+            // 
+            this.labelVersion.AutoSize = true;
+            this.labelVersion.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelVersion.Location = new System.Drawing.Point(240, 112);
+            this.labelVersion.Margin = new System.Windows.Forms.Padding(9, 0, 4, 0);
+            this.labelVersion.MaximumSize = new System.Drawing.Size(0, 49);
+            this.labelVersion.Name = "labelVersion";
+            this.labelVersion.Size = new System.Drawing.Size(457, 49);
+            this.labelVersion.TabIndex = 0;
+            this.labelVersion.Text = "2024.04";
+            this.labelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxDescription.Location = new System.Drawing.Point(240, 231);
+            this.textBoxDescription.Margin = new System.Windows.Forms.Padding(9, 5, 4, 5);
+            this.textBoxDescription.Multiline = true;
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.ReadOnly = true;
+            this.textBoxDescription.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxDescription.Size = new System.Drawing.Size(457, 277);
+            this.textBoxDescription.TabIndex = 23;
+            this.textBoxDescription.TabStop = false;
+            this.textBoxDescription.Text = "Carstarphen, Shamar\r\nChaney, Tyas\r\nKnight, Tony\r\nSawyer, Josh";
+            this.textBoxDescription.TextChanged += new System.EventHandler(this.textBoxDescription_TextChanged);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(234, 516);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 24;
+            this.btnClose.Text = "Close\r\n";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // frmAbout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 600);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmAbout";
+            this.Padding = new System.Windows.Forms.Padding(14);
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "About...";
+            this.Load += new System.EventHandler(this.frmAbout_Load);
+            this.tableLayoutPanel.ResumeLayout(false);
+            this.tableLayoutPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+
+        #endregion
+
+        private Button btnClose;
+    }
+}
---
+++ Draft2/Draft2/frmMain.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EduCal {
+	public partial class frmMain : Form {
+        // Static instance property
+        private static frmMain _instance;
+        public static frmMain Instance
+        {
+            get
+            {
+                if (_instance == null || _instance.IsDisposed)
+                {
+                    _instance = new frmMain();
+                }
+                return _instance;
+            }
+        }
+
+        // You might have an events list or similar data structure to store events
+        private List<EventForm> eventsList = new List<EventForm>();
+
+        // Method to add an event to the events list
+        public void AddEventToList(EventForm newEvent)
+        {
+            eventsList.Add(newEvent);
+
+            // Implement any additional logic you need
+            // For example, update the UI to reflect the new event
+        }
+        private void NewCalendar_Click(object sender, EventArgs e)
+        {
+            // Create and show the CalendarForm when "New Calendar" is clicked
+            Calendar calendar = new Calendar();
+            calendar.Show();
+            calendar.BuildNewCalendar(); // Build the new calendar
+        }
+        public frmMain()
+        {
+            InitializeComponent();
+            InitializeMenu();
+        }
+
+        private void InitializeMenu()
+        {
+            MenuStrip menuStrip = new MenuStrip();
+
+            ToolStripMenuItem fileMenu = new ToolStripMenuItem("File");
+
+
+            ToolStripMenuItem newCalendarItem = new ToolStripMenuItem("New Calendar");
+            newCalendarItem.Click += NewCalendar_Click;
+
+            ToolStripMenuItem newEventItem = new ToolStripMenuItem("New Event");
+
+            newEventItem.Click += NewEvent_Click;
+
+            ToolStripMenuItem openFileItem = new ToolStripMenuItem("Open");
+            openFileItem.Click += OpenFile_Click;
+            ToolStripMenuItem saveFileItem = new ToolStripMenuItem("Save");
+            saveFileItem.Click += SaveFile_Click;
+
+
+            ToolStripMenuItem saveAsFileItem = new ToolStripMenuItem("Save As...");
+            saveAsFileItem.Click += SaveAsFile_Click; // Event handler for Save As
+
+            ToolStripMenuItem exitItem = new ToolStripMenuItem("Exit");
+            exitItem.Click += Exit_Click; // Event handler for Exit
+
+            fileMenu.DropDownItems.Add(newCalendarItem);
+            fileMenu.DropDownItems.Add(newEventItem);
+            fileMenu.DropDownItems.Add(openFileItem);
+            fileMenu.DropDownItems.Add(saveFileItem);
+            fileMenu.DropDownItems.Add(saveAsFileItem); // Add Save As button
+            fileMenu.DropDownItems.Add(exitItem); // Add Exit button
+
+            menuStrip.Items.Add(fileMenu);
+
+
+
+            ToolStripMenuItem editMenu = new ToolStripMenuItem("Edit");
+
+            ToolStripMenuItem eventForm = new ToolStripMenuItem("Event Form");
+            eventForm.Click += NewEventForm_Click;
+            ToolStripMenuItem settings = new ToolStripMenuItem("Settings");
+            settings.Click += NewSettings_Click;
+
+            editMenu.DropDownItems.Add(eventForm);
+            editMenu.DropDownItems.Add(settings);
+
+            menuStrip.Items.Add(editMenu);
+            ToolStripMenuItem aboutMenu = new ToolStripMenuItem("About");
+
+            ToolStripMenuItem aboutForm = new ToolStripMenuItem("About Form");
+            aboutForm.Click += NewAboutForm_Click;
+            aboutMenu.DropDownItems.Add(aboutForm);
+            menuStrip.Items.Add(aboutMenu);
+
+            this.Controls.Add(menuStrip);
+
+
+        }
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            // Display a confirmation dialog before exiting
+            DialogResult result = MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            // Check the user's response
+            if (result == DialogResult.Yes)
+            {
+                // Exit the application
+                Application.Exit();
+            }
+        }
+
+        private void SaveAsFile_Click(object sender, EventArgs e)
+        {
+            // Create a SaveFileDialog instance
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            // Set properties of the save file dialog
+            saveFileDialog.Filter = "iCal files (*.ical)|*.ical|All files (*.*)|*.*";
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.RestoreDirectory = true;
+
+            // Show the save file dialog
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Get the selected file name
+                string fileName = saveFileDialog.FileName;
+
+                // Call the method to save the calendar data to the selected file
+                // Replace calendarData with the actual calendar data you want to save
+                // ExportHelper.ExportToICal(fileName, calendarData);
+            }
+        }
+
+        private void NewEvent_Click(object sender, EventArgs e)
+        {
+            // Implement logic for creating a new Event
+            EventForm eventForm = new EventForm(); // Assuming you have an EventForm for data entry
+            if (eventForm.ShowDialog() == DialogResult.OK)
+            {
+                EventForm newEvent = eventForm.GetEvent(); // Implement a method in EventForm to get the created event
+                events.Add(newEvent);
+            }
+        }
+        private void NewEventForm_Click(object sender, EventArgs e)
+        {
+            // Implement logic for creating a new Event
+            EventForm eventForm = new EventForm(); // Assuming you have an EventForm for data entry
+            if (eventForm.ShowDialog() == DialogResult.OK)
+            {
+                EventForm newEvent = eventForm.GetEvent(); // Implement a method in EventForm to get the created event
+                events.Add(newEvent);
+            }
+        }
+        private void NewSettings_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the SettingsForm
+            SettingsForm settingsForm = new SettingsForm();
+
+            // Show the SettingsForm
+            settingsForm.Show();
+        }
+
+
+        private void OpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Implement logic for opening the file
+                MessageBox.Show($"Opening file: {openFileDialog.FileName}");
+            }
+        }
+        private void NewAboutForm_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the SettingsForm
+            frmAbout aboutForm = new frmAbout();
+            aboutForm.Show();
+        }
+        private void SaveFile_Click(object sender, EventArgs e)
+        {
+            // TODO: Replace 'yourCalendarData' with the actual variable containing your calendar data
+            List<EventForm> yourCalendarData = events; // Replace this line
+
+            string fileName = "calendar.json"; // Provide the desired file path
+
+            FileHelper.SaveToFile(fileName, yourCalendarData);
+            MessageBox.Show("File saved successfully");
+        }
+
+    }
+}
